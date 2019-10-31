6b09fc26c475a967da403dcc76fd2746701db6ff
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-10-31 14:14:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,3 +11,15 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 display_board(board)
 display_board(board)
 display_board(board)
+
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  # ETC, an array for each win combination
+]
